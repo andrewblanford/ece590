14f1ac85d7015775abac3827a5e7aa9efa650b90
Andrew Blanford <ablanfor@gmu.edu>
2015-12-10 23:02:04 -0500

    added camera sensor

---
+++ project/videoTest.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# /* -*-  indent-tabs-mode:t; tab-width: 8; c-basic-offset: 8  -*- */
+# /*
+# Copyright (c) 2014, Daniel M. Lofaro <dan (at) danLofaro (dot) com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above copyright
+#       notice, this list of conditions and the following disclaimer in the
+#       documentation and/or other materials provided with the distribution.
+#     * Neither the name of the author nor the names of its contributors may
+#       be used to endorse or promote products derived from this software
+#       without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# */
+import pt_ach
+import ach
+import sys
+import time
+from ctypes import *
+import socket
+import cv2.cv as cv
+import cv2
+import numpy as np
+import math
+
+# CV setup 
+cv.NamedWindow("wctrl", cv.CV_WINDOW_AUTOSIZE)
+
+# added
+##sock.connect((MCAST_GRP, MCAST_PORT))
+newx = 640
+newy = 480
+
+nx = 320
+ny = 240
+
+vl = ach.Channel(pt_ach.ROBOT_VID_CHAN)
+vl.flush()
+
+while True:
+    # Get Frame
+    imgL = np.zeros((newx,newy,3), np.uint8)
+    c_image = imgL.copy()
+    vidL = cv2.resize(c_image,(newx,newy))
+    [status, framesize] = vl.get(vidL, wait=False, last=True)
+    if status == ach.ACH_OK or status == ach.ACH_MISSED_FRAME or status == ach.ACH_STALE_FRAMES:
+        vid2 = cv2.resize(vidL,(nx,ny))
+        #imgL = cv2.cvtColor(vid2,cv2.COLOR_BGR2RGB)
+        cv2.imshow("wctrl", imgL)
+        cv2.waitKey(10)
+    else:
+        raise ach.AchException( v.result_string(status) )
